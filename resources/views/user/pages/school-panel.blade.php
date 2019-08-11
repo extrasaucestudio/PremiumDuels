61aac794ffa822695c67a4bf4addd580cd169f44
PitchRE <pitch@redvelvet.pro>
2019-08-11 02:58:07 +0200

    https exception fix

--- resources/views/user/pages/school-panel.blade.php
+++ resources/views/user/pages/school-panel.blade.php
@@ -8,8 +8,8 @@
 
 <head>
 
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
 </head>
 
 <body>
