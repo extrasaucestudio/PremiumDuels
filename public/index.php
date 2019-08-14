947d1d2a622373f9a68b8a91464facaf31fab9b0
PitchRE <pitch@redvelvet.pro>
2019-08-14 15:52:08 +0200

    revert changes.

--- public/index.php
+++ public/index.php
@@ -1,17 +1,4 @@
 <?php
-header_remove();
-function set_content_length($output)
-{
-    header("Content-Length: " . strlen($output));
-    return $output;
-}
-ob_start("set_content_length");
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
 
 define('LARAVEL_START', microtime(true));
 
