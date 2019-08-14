2a098e851631a311fa45b76bc5d794a2ff42909d
PitchRE <pitch@redvelvet.pro>
2019-08-14 15:27:40 +0200

    ob flush

--- app/Http/Controllers/WarbandApiController.php
+++ app/Http/Controllers/WarbandApiController.php
@@ -59,13 +59,8 @@ class WarbandApiController extends Controller
 
 
             $user->touch();
-
-            $resp = "1|3|{$request->player_id}|{$user->uid}|{$user->secret_key}|{$user->elo}|{$helmet}|{$armor}|{$gloves}|{$boots}|{$weapon}|Joining message string there";
-            return response($resp)
-                ->withHeaders([
-                    'Content-Type' => 'text/plain'
-
-                ]);
+            ob_flush();
+            return "1|3|{$request->player_id}|{$user->uid}|{$user->secret_key}|{$user->elo}|{$helmet}|{$armor}|{$gloves}|{$boots}|{$weapon}|Joining message string there";
         }
     }
 
