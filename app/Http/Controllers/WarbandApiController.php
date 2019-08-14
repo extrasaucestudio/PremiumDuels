406bc0329eca27039b9f9714e30fa438cd4b6b4a
PitchRE <pitch@redvelvet.pro>
2019-08-14 16:02:48 +0200

    test

--- app/Http/Controllers/WarbandApiController.php
+++ app/Http/Controllers/WarbandApiController.php
@@ -48,7 +48,8 @@ class WarbandApiController extends Controller
             $user->touch();
             $resp = "|1|3|{$request->player_id}|{$user->uid}|{$user->secret_key}|{$user->elo}|{$helmet}|{$armor}|{$gloves}|{$boots}|{$weapon}|Joining message string there|";
 
-            return;
+            return response('1', 200)
+                ->header('Content-Type', 'text/plain');
         }
     }
 
