94a857dfb9ef14fc5f02f7cdd928bd57237ccd9c
Pitch <pitch@redvelvet.pro>
2019-08-17 14:35:34 +0200

    Revert "Extra (#5)" (#6)
    This reverts commit 2f86e50daa7a36369fcaacfb2a71dc19ab295769.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        'localhostOnly' => \App\Http\Middleware\localhostOnly::class,
     ];
 
     /**
