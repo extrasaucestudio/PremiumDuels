3f2d8e7a1635af867fffb5bb362e2a432a1f1fc0
PitchRE <pitch@redvelvet.pro>
2019-08-21 21:11:06 +0200

    update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -54,27 +54,15 @@
             </div>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
+            <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Rank
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Rank Progress
                             </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$user->title->name}}
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar"
-                                            style="width: {{$nextRank->percent}}%"
-                                            aria-valuenow="{{$nextRank->percent}}" aria-valuemin="0"
-                                            aria-valuemax="100"></div>
-                                    </div>
-                                </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$user->Title->name}} |
+                                {{$nextRank->percent}}%
                             </div>
                         </div>
                         <div class="col-auto">
@@ -84,6 +72,8 @@
                 </div>
             </div>
         </div>
+
+
 
         <!-- Pending Requests Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
--- resources/views/user/pages/user-profile.blade.php
+++ resources/views/user/pages/user-profile.blade.php
@@ -69,7 +69,7 @@
             </div>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
+        {{-- <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
@@ -81,53 +81,70 @@
                                 <div class="col-auto">
                                     <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
                                         {{$foreign_user->title->name}}
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar"
-                                            style="width: {{$nextRank->percent}}%"
-                                            aria-valuenow="{{$nextRank->percent}}" aria-valuemin="0"
-                                            aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-fist-raised fa-2x text-gray-300"></i>
-                        </div>
+    </div>
+</div>
+<div class="col">
+    <div class="progress progress-sm mr-2">
+        <div class="progress-bar bg-info" role="progressbar" style="width: {{$nextRank->percent}}%"
+            aria-valuenow="{{$nextRank->percent}}" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+</div>
+</div>
+</div>
+<div class="col-auto">
+    <i class="fas fa-fist-raised fa-2x text-gray-300"></i>
+</div>
+</div>
+</div>
+</div>
+</div> --}}
+
+
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Rank Progress
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$user->Title->name}} | {{$nextRank->percent}}%
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">School
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                @if($foreign_user->School != null)
-                                <a href="/school/{{$foreign_user->School->MemberToSchool->id}}" style="  color:gray !important;
-                                text-decoration:none;">
-                                    {{$foreign_user->School->MemberToSchool->name}}
-                                </a>
-                                @else
-                                None
-                                @endif
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
+                <div class="col-auto">
+                    <i class="fas fa-fist-raised fa-2x text-gray-300"></i>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<!-- Pending Requests Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-warning shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">School
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        @if($foreign_user->School != null)
+                        <a href="/school/{{$foreign_user->School->MemberToSchool->id}}" style="  color:gray !important;
+                                text-decoration:none;">
+                            {{$foreign_user->School->MemberToSchool->name}}
+                        </a>
+                        @else
+                        None
+                        @endif
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 
 
 </div>
